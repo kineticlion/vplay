a9b609c8432bbd07e1472c55444ca8eb408a7764
Sufiyan <sufiyansaboowala13@gmail.com>
2020-05-20 13:36:37 -0400

    Added spinner for keys

--- src/components/Keyboard/Keys/Keys.js
+++ src/components/Keyboard/Keys/Keys.js
@@ -1,15 +1,33 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Key from "./Key";
 import "./keys.css";
 import { KeyboardContext } from "../contexts/KeyboardCtxProvider";
+import Spinner from "react-bootstrap/Spinner";
 const Keys = ({ octaves, startOctave }) => {
-  const { keys, instrument, buildKeys } = useContext(KeyboardContext);
+  const { keys, instrument, buildKeys, instrumentName } = useContext(
+    KeyboardContext
+  );
+  const [isLoading, setIsLoading] = useState();
+  useEffect(() => {
+    setIsLoading(true);
+  }, [instrumentName]);
 
   useEffect(() => {
     buildKeys(startOctave, octaves);
+    setIsLoading(false);
   }, [instrument]);
 
-  return (
+  return isLoading ? (
+    <Spinner
+      animation="border"
+      style={{
+        padding: 45,
+        fontSize: 10,
+        marginLeft: "35.4vw",
+        marginTop: "2vh",
+      }}
+    />
+  ) : (
     <>
       {keys.map((key, index) => (
         <Key key={index} note={key.note} accidental={key.isAccidental} />
